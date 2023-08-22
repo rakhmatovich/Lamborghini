f00484722e3106fca04cf7b8600a3dbc91404f3f
rakhmatovich <nurmuxammadrahmatov@gmail.com>
2023-08-22 11:39:40 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 function App() {
 
   return (
-   <div className="w-full h-screen flex items-center justify-center bg-blue-200">
-       <h1 className="text-[40px] text-blue-600 font-bold">Hello Lamborghini</h1>
+   <div className="w-full h-screen flex items-center justify-center bg-gray-900">
+       <h1 className="text-[40px] text-gray-500 font-bold">Hello Lamborghini</h1>
    </div>
   )
 }
